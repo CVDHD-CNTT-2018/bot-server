801ae2c98be72849de2070f3876f81ecb9d82f5f
Thanh Tùng <thanhtung.uet@gmail.com>
2018-10-05 13:02:57 +0700

    Change send API to ver2.6

--- src/APIs/message.js
+++ src/APIs/message.js
@@ -1,14 +1,14 @@
 // @flow
 
 import type {AxiosError, AxiosResponse} from 'axios';
-import '../dotenv';
 import {request} from '../helpers/request';
+import '../dotenv';
 
 const {PAGE_ACCESS_TOKEN} = process.env;
 
 class Message {
   send: Function = (message: Object): Promise => {
-    return request.post('/v3.1/me/messages', message, {
+    return request.post('/v2.6/me/messages', message, {
       params: {
         access_token: PAGE_ACCESS_TOKEN
       }
@@ -29,14 +29,14 @@ class Message {
   };
 
   receive: Function = (event: Object) => {
-    const {sender, recipient, message} = event;
+    const {sender, message} = event;
     const {mid, app_id, metadata} = message;
     if (message.is_echo) {
       console.log("Received echo for message %s and app %d with metadata %s", mid, app_id, metadata);
     }
     if (message.text) {
       this.send({
-        recipient,
+        recipient: sender,
         message: {
           text: `Nhận được tin nhắn: ${message.text}`,
           metadata: "TEXT_MESSAGE"
