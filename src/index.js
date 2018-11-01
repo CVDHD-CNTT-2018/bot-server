413331e9a56d37c467fb94a811e540edbec1d8d9
Thanh Tùng <thanhtung.uet@gmail.com>
2018-11-01 22:38:44 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 // @flow
 
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+
 import app from './app';
 import './dotenv';
 
-const callback: Function = (error: Error) => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('Web server is up and running!');
+const {HTTP_PORT, HTTPS_PORT} = process.env;
+
+const credentials = {
+  key: fs.readFileSync('.ssl/key'),
+  cert: fs.readFileSync('.ssl/cert')
 };
 
-const PORT = process.env.PORT || 8080;
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-app.listen(PORT, callback);
+try {
+  httpServer.listen(HTTP_PORT);
+  console.log('HTTP server is running on port %d', HTTP_PORT);
+  httpsServer.listen(HTTPS_PORT);
+  console.log('HTTPS server is running on port %d', HTTPS_PORT);
+} catch (error) {
+  console.error(error);
+}
